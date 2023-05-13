ef48899cf83cc2a70b396ff826711e4fe8e1dada
Roshan Jha <125554364+roshanjha26@users.noreply.github.com>
2023-05-13 12:32:13 +0530

    Update userController.js

--- controllers/userController.js
+++ controllers/userController.js
@@ -278,7 +278,10 @@ export const deleteMyProfile = catchAsyncError(async (req, res, next) => {
   res
     .status(200)
     .cookie("token", null, {
-      expires: new Date(Date.now()),
+      expires: new Date(Date.now()),   
+      httpOnly: true,
+      secure: true,
+      sameSite: "none",
     })
     .json({
       success: true,
